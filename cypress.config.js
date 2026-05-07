94bd55afbe25eca3366558b91af7f8141207d040
Alex Mariano <alexdealm@gmail.com>
2026-05-07 17:06:19 -0300

    fix: padroniza sintaxe do cypress.config

--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,6 @@ const { allureCypress } = require("allure-cypress/reporter");
 
 module.exports = defineConfig({
   e2e: {
-    allowCypressEnv: false, // <-- Remove aquele Warning de segurança do terminal
     setupNodeEvents(on, config) {
       allureCypress(on, config, {
         resultsDir: "allure-results",
